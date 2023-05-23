9d5910c2bd91979c87de7a6ce9f1bae972ec8d37
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-23 11:38:51 -0300

    add checked_add and checked_mul

--- increment_overflow_checks/src/lib.rs
+++ increment_overflow_checks/src/lib.rs
@@ -27,6 +27,26 @@ impl IncrementContract {
         count
     }
 
+    /// Increment increments an internal counter, and returns the value.
+    pub fn checked_increment(env: Env, n: i128) -> i128 {
+        // Get the current count.
+        let mut count: i128 = env
+            .storage()
+            .get(&COUNTER)
+            .unwrap_or(Ok(0)) // If no value set, assume 0.
+            .unwrap(); // Panic if the value of COUNTER is not i128.
+        log!(&env, "count: {}", count);
+
+        // Increment the count.
+        count = count.checked_add(n).unwrap();
+
+        // Save the count.
+        env.storage().set(&COUNTER, &count);
+
+        // Return the count to the caller.
+        count
+    }
+
     /// Decrement decrements an internal counter, and returns the value.
     pub fn decrement(env: Env, n: i128) -> i128 {
         // Get the current count.
@@ -39,6 +59,46 @@ impl IncrementContract {
 
         // Decrement the count.
         count -= n;
+
+        // Save the count.
+        env.storage().set(&COUNTER, &count);
+
+        // Return the count to the caller.
+        count
+    }
+
+    /// Mul multiplies an internal counter, and returns the value.
+    pub fn mul(env: Env, n: i128) -> i128 {
+        // Get the current count.
+        let mut count: i128 = env
+            .storage()
+            .get(&COUNTER)
+            .unwrap_or(Ok(0)) // If no value set, assume 0.
+            .unwrap(); // Panic if the value of COUNTER is not i128.
+        log!(&env, "count: {}", count);
+
+        // Decrement the count.
+        count = count * n;
+
+        // Save the count.
+        env.storage().set(&COUNTER, &count);
+
+        // Return the count to the caller.
+        count
+    }
+
+    /// Mul multiplies with CHECKED_MUL
+    pub fn checked_mul(env: Env, n: i128) -> i128 {
+        // Get the current count.
+        let mut count: i128 = env
+            .storage()
+            .get(&COUNTER)
+            .unwrap_or(Ok(0)) // If no value set, assume 0.
+            .unwrap(); // Panic if the value of COUNTER is not i128.
+        log!(&env, "count: {}", count);
+
+        // Decrement the count.
+        count = count.checked_mul(n).unwrap();
 
         // Save the count.
         env.storage().set(&COUNTER, &count);
