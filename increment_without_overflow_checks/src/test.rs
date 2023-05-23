c5160532d4f2b745560c97fd34561a534041dfc3
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-23 11:39:59 -0300

    add checked_add and checked_mul

--- increment_without_overflow_checks/src/test.rs
+++ increment_without_overflow_checks/src/test.rs
@@ -16,6 +16,9 @@ fn test() {
     assert_eq!(client.increment(&1), 2);
     assert_eq!(client.increment(&1), 3);
     assert_eq!(client.decrement(&1), 2);
+    assert_eq!(client.mul(&2), 4);
+    assert_eq!(client.checked_mul(&2), 8);
+    assert_eq!(client.checked_increment(&1), 9);
 }
 
 #[test]
@@ -26,11 +29,13 @@ fn test_should_work_with_max() {
 
     let max_value: i128 = i128::MAX;
     assert_eq!(client.increment(&max_value), max_value);
+    assert_eq!(client.mul(&1), max_value);
+    assert_eq!(client.checked_mul(&1), max_value);
 }
 
 #[test]
 #[should_panic(expected = "")]
-fn should_panic_overflow() {
+fn increment_should_panic_overflow() {
     let env = Env::default();
     let contract_id = env.register_contract(None, IncrementContract);
     let client = IncrementContractClient::new(&env, &contract_id);
@@ -39,8 +44,45 @@ fn should_panic_overflow() {
     assert_eq!(client.increment(&1), 1);
     let max_value: i128 = i128::MAX;
     client.increment(&max_value);
+}
+
+#[test]
+#[should_panic(expected = "")]
+fn checked_increment_should_panic_overflow() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, IncrementContract);
+    let client = IncrementContractClient::new(&env, &contract_id);
+
     
+    assert_eq!(client.increment(&1), 1);
+    let max_value: i128 = i128::MAX;
+    client.checked_increment(&max_value);
+}
+
+#[test]
+#[should_panic(expected = "")]
+fn mul_should_panic_overflow() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, IncrementContract);
+    let client = IncrementContractClient::new(&env, &contract_id);
+
     
+    let max_value: i128 = i128::MAX;
+    assert_eq!(client.increment(&max_value), max_value);
+    client.mul(&2);
+}
+
+#[test]
+#[should_panic(expected = "")]
+fn checked_mul_should_panic_overflow() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, IncrementContract);
+    let client = IncrementContractClient::new(&env, &contract_id);
+
+    
+    let max_value: i128 = i128::MAX;
+    assert_eq!(client.increment(&max_value), max_value);
+    client.checked_mul(&2);
 }
 
 #[test]
