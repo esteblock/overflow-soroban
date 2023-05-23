ef441255c9e0d6f1544610c8ea8fbd6c30677323
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-23 11:22:57 -0300

    Add decrement function

--- increment_overflow_checks/src/lib.rs
+++ increment_overflow_checks/src/lib.rs
@@ -26,6 +26,26 @@ impl IncrementContract {
         // Return the count to the caller.
         count
     }
+
+    /// Decrement decrements an internal counter, and returns the value.
+    pub fn decrement(env: Env, n: u32) -> u32 {
+        // Get the current count.
+        let mut count: u32 = env
+            .storage()
+            .get(&COUNTER)
+            .unwrap_or(Ok(0)) // If no value set, assume 0.
+            .unwrap(); // Panic if the value of COUNTER is not u32.
+        log!(&env, "count: {}", count);
+
+        // Decrement the count.
+        count -= n;
+
+        // Save the count.
+        env.storage().set(&COUNTER, &count);
+
+        // Return the count to the caller.
+        count
+    }
 }
 
 mod test;
--- increment_overflow_checks/src/test.rs
+++ increment_overflow_checks/src/test.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 
 use super::{IncrementContract, IncrementContractClient};
-use soroban_sdk::{testutils::Logger, Env};
+use soroban_sdk::{Env};
 
 extern crate std;
 
@@ -15,6 +15,7 @@ fn test() {
     assert_eq!(client.increment(&1), 1);
     assert_eq!(client.increment(&1), 2);
     assert_eq!(client.increment(&1), 3);
+    assert_eq!(client.decrement(&1), 2);
 }
 
 #[test]
@@ -40,5 +41,17 @@ fn should_panic_overflow() {
     client.increment(&max_value);
     
     
-    std::println!("{}", env.logger().all().join("\n"));
+}
+
+#[test]
+#[should_panic(expected = "")]
+fn should_panic_underflow() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, IncrementContract);
+    let client = IncrementContractClient::new(&env, &contract_id);
+
+    
+    assert_eq!(client.increment(&1), 1);
+    let max_value: u32 = u32::MAX;
+    assert_eq!(client.decrement(&2), max_value);
 }
